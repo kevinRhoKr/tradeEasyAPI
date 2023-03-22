b1f17b3e045d66b3578839035922bf24e56819fe
kevinRho <kevinrho51@gmail.com>
2023-03-21 21:24:34 -0400

    comments

--- app/api/views.py
+++ app/api/views.py
@@ -1,8 +1,40 @@
+from . import api
+from ..models import User, Item, Conversation
+from flask import jsonify, request, url_for
+from .. import db
+from ..errors import unauthorized, not_found, forbidden, bad_request
+from flask_jwt_extended import get_jwt_identity, jwt_required
 
 # TODO: Start writing the api endpoints
 # api endpoints:
     # get all the posts that the user has created
-    # get all the posts that the user has indicated liked
-    # Show items on the market place that the user hasn't seen yet based on locality setting
+    # Get all the items based on the locality setting that the user has been set with
+    # Change the locality
+
+
+
     # Initiate the chatting system (research more on whether this should be on the api side or on the client side)
-    #
+
+
+
+#TODO: JONATHAN
+@api.route("/myposts/")
+@jwt_required()
+def getMyPosts():
+    pass
+
+
+#TODO: RISHIKESH
+@api.route("/getItems/")
+@jwt_required()
+def getItems():
+    pass
+
+
+#TODO: RYAN
+@api.route("/changeLocation/")
+@jwt_required()
+def changeLocation():
+    pass
+
+
